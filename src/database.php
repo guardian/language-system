6cb736e259cfae94893402b77cceb6aa3118c829
Dave Allison <david.allison@theguardian.com>
2018-10-30 16:26:48 +0000

    Fix for missing )

--- src/database.php
+++ src/database.php
@@ -1,6 +1,6 @@
 <?php
 // Connects to your Database
 
-mysql_connect(getenv('DATABASE_HOST'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD') or die(mysql_error());
+mysql_connect(getenv('DATABASE_HOST'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD')) or die(mysql_error());
 mysql_select_db("language_system") or die(mysql_error());
 ?>
