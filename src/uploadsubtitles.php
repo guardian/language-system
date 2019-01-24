1a2bcd12331808cd1596ef85a384d2d652524fc1
Dave Allison <david.allison@theguardian.com>
2019-01-24 17:48:18 +0000

    Replace spaces in subtitle file names with underscores and only allow upload of SRT files

--- src/uploadsubtitles.php
+++ src/uploadsubtitles.php
@@ -1,7 +1,8 @@
 <?php
 include 'database.php';
 $target_dir = getenv('LANGUAGE_SUBTITLES')."/".$_GET['id']."/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$final_name = str_replace(" ", "_", basename($_FILES["fileToUpload"]["name"]));
+$target_file = $target_dir . $final_name;
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -19,9 +20,8 @@ if ($_FILES["fileToUpload"]["size"] > 5000000000000000000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "srt" && $imageFileType != "mxf" && $imageFileType != "mov"
-&& $imageFileType != "mpg" ) {
-    echo "Sorry, only SRT, MXF, MOV & MPG files are allowed.";
+if($imageFileType != "srt" && $imageFileType != "SRT") {
+    echo "Sorry, only SRT files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -30,15 +30,13 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". $final_name. " has been uploaded.";
 		header("Location: media.php?id=".$_GET['id']);
    		exit;
-        
         
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
-
 
 ?>
